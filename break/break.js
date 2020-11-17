735a56874430a9357c9259054c00da7d6f43201a
Erwin Henraat <e.henraat@ma-web.nl>
2020-11-17 13:44:12 +0100

    Gimme a break example

--- break/break.js
+++ break/break.js
@@ -3,7 +3,7 @@ p = document.getElementById("clock")
 
 breakStart = new Date()
 breakEnd = new Date()
-
+//stel de begin en eindtijd van je break hier in
 
 e = setInterval(updateTime,1000) //code zorgt dat de update time functie elke seconde wordt uitgevoerd
 
@@ -11,14 +11,14 @@ function updateTime(){
     d = new Date()
      p.innerHTML = "Break Time from "+breakStart.toTimeString()+" until " +breakEnd.toTimeString()+"<br/> Time: "+d.getHours() +" : "+ d.getMinutes() +" : "+ d.getSeconds()
    
-   
+   //check hier hoe laat het is en of je wel of geen break moet hebben
 }
 function breakNow(){
-    
+    //pas hier de image aan voor als je een break hebt
    
 }
 function endBreak(){
-   
+   //Pas hier de image aan voor als je aan het werk bent
    
 }
 
