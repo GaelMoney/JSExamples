c03b5b11db605a809c88c060b9d2d0183f60b307
Erwin Henraat <e.henraat@ma-web.nl>
2020-11-17 15:40:22 +0100

    grid

--- grid/grid.js
+++ grid/grid.js
@@ -1,16 +1,16 @@
-cols = document.getElementById("cols")
+cols = document.getElementById("cols")                              //get DOM elements
 size = document.getElementById("size")
 grid = document.getElementById("grid")
 
-cols.addEventListener("change", update)
+cols.addEventListener("change", update)                             //call update when user changes value
 size.addEventListener("change", update)
 
 function update(){
-    grid.innerHTML = ""
-    for(i=0;i<size.value;i++){
-        console.log(cols.value)
-        if(i % cols.value == 0 && i!=0)grid.innerHTML+="<br>"
-        grid.innerHTML += "X"
+    grid.innerHTML = ""                                             //clear grid
+    for(i=0;i<size.value;i++){                                      //loop according to size
+        
+        if(i % cols.value == 0 && i!=0)grid.innerHTML+="<br>"       //Add <br> when grid item is devidable by number of collums
+        grid.innerHTML += "X"                                       //Add 'X' for every griditem
 
     }
 }
