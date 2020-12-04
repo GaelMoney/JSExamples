a7ffed25eecbf037dcdee29629d2e372aa31da7d
Erwin Henraat <e.henraat@ma-web.nl>
2020-12-04 12:20:59 +0100

    arrays assignment obscured

--- arrays/array.js
+++ arrays/array.js
@@ -1,85 +1,40 @@
-
-
-let toDos = ["feed the cat","wash the windows","Fuel the car"];
-let finished = [false,false,false];
-
-
-let btn_add = document.getElementById("add");
-let btn_finish = document.getElementById("finish");
-let btn_remove = document.getElementById("remove");
-
-let inputValue = document.getElementById("input");
-
-let listing = document.getElementById("listing");
-
-updateListing();
+//Create your arrays an variables
+//get DOM elements
 
 btn_add.addEventListener("click", ()=>{
-    //handle add
-    if(inputValue.value != ""){
-        toDos.push(inputValue.value);
-        finished.push(false);
-        updateListing();
-        inputValue.value = "";
-    }else{
-        alert("enter a new to do!");
-    }
+    //handle adding a new todo
+
 });
 
 btn_finish.addEventListener("click", ()=>{
-    //handle finish
-    i = Number(inputValue.value);
+    //handle finishing a todo
     
-    if(isNaN(i)){
-        alert("vul een nummer in");
+    //make sure your input is seen as a number
+    x = Number( );
+    //check if input really is a number
+    if(isNaN(x)){
+        //not a number
     }else{
-        if(i < finished.length){
-            console.log("finish "+i);
-            finished[i] = true;
-        }
-        else{
-            alert("geen geldige index");
-        }
+        //is a number
     }
 
-    updateListing();
 });
 
 btn_remove.addEventListener("click", ()=>{
-    //handle romove
-    i = Number(inputValue.value);
-    if(isNaN(i)){
-        alert("vul een nummer in");
-    }else{
-        if(i < finished.length){
-            console.log("remove "+i);
-            finished.splice(i,1);
-            toDos.splice(i,1);
-        }
-        else{
-            alert("geen geldige index");
-        }
-    }
-    updateListing();
+    //handle removing of a todo
 
+    //make sure your input is seen as a number
+    x = Number( );
+    //check if input really is a number
+    if(isNaN(x)){
+        //not a number
+    }else{
+        //is a number
+    }
 });
 
-
-
 function updateListing(){
-    listing.innerHTML = "<ol>";
-
-    for(i = 0; i< toDos.length; i++){
-        if(finished[i]){
-            listing.innerHTML += "<li><del>"+toDos[i]+"</del></li>";            
-        }else{
-            listing.innerHTML += "<li>"+toDos[i]+"</li>";
-        }
-
-
-    }
-    listing.innerHTML += "</ol>";
-
-    
-     
+ //print list of todo's
+ //use loop to step trough the arrays
+   
 }
